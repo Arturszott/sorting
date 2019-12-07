a361b8436b5aa8fb4a42086681405d67f9e5e1e4
Artur Szott <artur.szott@holidaycheck.com>
2019-12-07 14:20:34 +0100

    Implement basic buble sort

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+// steps
+// Show array
+
+// run algorithm on click
+// add operations to queue
+// play queue
+
+// operations: Compare, Swap,
+
+const isNextBigger = (array, i) => array[i + 1] > array[i];
+const isNextSmaller = (array, i) => {
+	const current = array[i];
+	const next = array[i + 1];
+
+	console.log(current, next, next < current);
+	return next < current;
+};
+
+const executeSwap = (pair, array) => ([ array[pair[0]], array[pair[1]] ] = [ array[pair[1]], array[pair[0]] ]);
+
+function sortBubble(array) {
+	const localArray = [ ...array ];
+	const operations = [];
+	let shouldSort = true;
+
+	while (shouldSort) {
+		let iterationsSwapsCount = 0;
+
+		for (let i = 0; i < localArray.length - 1; i++) {
+			if (isNextSmaller(localArray, i)) {
+				const swapPair = [ i, i + 1 ];
+
+				iterationsSwapsCount++;
+				operations.push(swapPair);
+				executeSwap(swapPair, localArray);
+			}
+		}
+
+		shouldSort = Boolean(iterationsSwapsCount);
+	}
+
+	return operations;
+}
+
+const initialArray = [ 2, 4, 3, 5, 1 ];
+const operations = sortBubble(initialArray);
+const generateSteps = (ops) => {
+	const localCopy = [ ...initialArray ];
+
+	return [
+		initialArray,
+		...ops.map((pair) => {
+			executeSwap(pair, localCopy);
+			return [ ...localCopy ];
+		})
+	];
+};
+const steps = generateSteps(operations);
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ array, setArray ] = useState(initialArray);
+
+	useEffect(() => {
+		for (let i = 0; i < steps.length; i++) {
+			window.setTimeout(() => {
+				setArray(steps[i]);
+			}, 500 * i);
+		}
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>{array}</h1>
+			</header>
+		</div>
+	);
 }
 
 export default App;
