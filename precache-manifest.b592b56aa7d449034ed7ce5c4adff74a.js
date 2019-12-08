e303dcef0605b07ebde92f698fc0386f0ef53126
Artur Szott <artur.szott@holidaycheck.com>
2019-12-08 18:15:32 +0100

    Updates

---
+++ precache-manifest.b592b56aa7d449034ed7ce5c4adff74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67c7c150211b955b4fb5d6a8b0165d3d",
+    "url": "/sorting/index.html"
+  },
+  {
+    "revision": "dd968c34fde7ab89dfdb",
+    "url": "/sorting/static/css/main.e06b02bb.chunk.css"
+  },
+  {
+    "revision": "09b124069cfecb09220c",
+    "url": "/sorting/static/js/2.310f55bb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sorting/static/js/2.310f55bb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd968c34fde7ab89dfdb",
+    "url": "/sorting/static/js/main.5cfd7d3f.chunk.js"
+  },
+  {
+    "revision": "392016f7d0557f519efe",
+    "url": "/sorting/static/js/runtime-main.95332809.js"
+  }
+]);
